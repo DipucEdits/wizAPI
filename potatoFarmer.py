7bff78e7104823981854096a4fdda9bf7dcd8643
DipucEdits <84294563+DipucEdits@users.noreply.github.com>
2021-05-16 16:15:20 -0700

    Update potatoFarmer.py
    Change meteor back to tempest

--- potatoFarmer.py
+++ potatoFarmer.py
@@ -51,8 +51,8 @@ while True:
     inFight = not player.is_idle()
     while inFight:
 
-        if player.enchant('meteor-strike', 'epic') or player.find_spell('meteor-strike-enchanted'):
-            player.cast_spell('meteor-strike-enchanted')
+        if player.enchant('tempest', 'colossal') or player.find_spell('tempest_enchanted'):
+            player.cast_spell('tempest-enchanted')
         else:
             player.pass_turn()
 
